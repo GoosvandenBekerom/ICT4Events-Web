ebd19fe62c80152d824df7b6d165125544509aa2
djdino56 <spongdino@gmail.com>
2016-01-05 21:39:53 +0100

    Reservtion models added

---
+++ src/SharedModels/Data/ContextInterfaces/IReservationContext.cs
@@ -0,0 +1,9 @@
+﻿using SharedModels.Models;
+
+namespace SharedModels.Data.ContextInterfaces
+{
+    public interface IReservationContext : IRepositoryContext<Reservation>
+    {
+        Reservation GetLastAdded();
+    }
+}
---
+++ src/SharedModels/Logic/ReservationLogic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using SharedModels.Data.ContextInterfaces;
+using SharedModels.Data.OracleContexts;
+using SharedModels.Models;
+
+namespace SharedModels.Logic
+{
+    public class ReservationLogic
+    {
+        private readonly IReservationContext _context;
+
+        public ReservationLogic(IReservationContext context)
+        {
+            _context = context;
+        }
+
+        public List<Reservation> GetAllReservations()
+        {
+            return _context.GetAll();
+        }
+
+        public Reservation GetByID(int id)
+        {
+            return _context.GetById(id);
+        }
+
+        public bool Insert(Reservation reservation)
+        {
+            return _context.Insert(reservation);
+        }
+
+        public Reservation GetLastAdded()
+        {
+            return _context.GetLastAdded();
+        }
+    }
+}
